ccc77c38fc3092a265633605ed0d1ae78eafb61a
ahmed <yasser.rohaim9@gmail.com>
2025-01-30 22:27:47 +0200

    implemented code error detection

--- runner.py
+++ runner.py
@@ -6,7 +6,13 @@ import re
 import signal
 from fastapi import FastAPI
 
-# Global flag to handle timeout exception
+def check_for_errors(output):
+    error_pattern = r"Traceback|Error|Exception"
+
+    if re.search(error_pattern, output, re.IGNORECASE):
+        return True
+    else:
+        return False
 class TimeoutException(Exception):
     pass
 
@@ -50,7 +56,8 @@ def read_output(master):
     return output
 
 def send_command(process, master, command):
-    """ Send a command to the Python subprocess and return the output, with a 2-second timeout """
+    """ Send a command to the Python subprocess and return the output,
+      with a 2-second timeout and 100 MB memory limit """
     command = "import resource;resource.setrlimit(resource.RLIMIT_AS, (100 * 1024 * 1024, 100 * 1024 * 1024));" + command
     
     # Set up the signal handler for the alarm
@@ -66,26 +73,31 @@ def send_command(process, master, command):
         # Cancel the alarm if the command finishes before the timeout
         signal.alarm(0)
         
-        # Remove echoed command from output
+      
         output_lines = output.splitlines()
         cleaned_output = []
         for line in output_lines:
             if line.strip() == command.strip():
                 continue  # Skip the echoed command
             cleaned_output.append(line)
-
-        return "\n".join(cleaned_output).strip()
-    except MemoryError:
-        "Error: memory limit exceeded."
+        stdout="\n".join(cleaned_output).strip()
+        if (len(cleaned_output) and cleaned_output[-1].startswith("MemoryError")): #check for memory error
+            return {"error":"memory limit exceeded"}
+        if  check_for_errors(stdout): # detect if an execution error occured
+            return {"stderr":stdout}
+        return {"stdout":stdout}
     except TimeoutException:
-        return "Error: Command execution exceeded time limit."
+        return {"error": "time limit exceeded"}
 
 # Spawn Python shell
 process, master = spawn_python_shell()
 
-# Example commands
+# tests
+'''
 print(send_command(process, master, 'print("hi")'))  # Should print "hi"
-print(send_command(process, master, 'x = 5'))  # Should be empty (variable assignment)
+print(send_command(process, master, 'x=5'))  # Should be empty (variable assignment)
 print(send_command(process, master, 'print(x * 2)'))  
+print(send_command(process, master, '1/0'))  
 print(send_command(process,master,'x="c"*100*2**20'))
 print(send_command(process, master, 'import time;time.sleep(4)'))
+'''
